68b6e4ee1511621d5bf8838e42623edcf9c6d768
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-09 00:45:20 -0300

    Add MyTetris game

---
+++ my_tetris/desktop/src/com/pfariasmunoz/mytetris/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pfariasmunoz.mytetris.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pfariasmunoz.mytetris.MyTetris;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyTetris(), config);
+	}
+}
