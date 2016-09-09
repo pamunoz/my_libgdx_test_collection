1e452b37c6745f7caa18acad25a9abe93474aae9
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-09 15:37:37 -0300

    Add a GlyphLayout implementation for the fonts

--- gamesfromscratch/core/src/com/pfariasmunoz/gfs/GFS.java
+++ gamesfromscratch/core/src/com/pfariasmunoz/gfs/GFS.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -13,10 +14,20 @@ public class GFS extends ApplicationAdapter {
 	Texture img;
 	Sprite sprite;
     BitmapFont font;
+    GlyphLayout glyphLayout;
     String myText;
+    String myText_2;
+    float xPos;
+    float yPos;
+
+
 	
 	@Override
 	public void create () {
+
+        xPos = Gdx.graphics.getWidth();
+        yPos = Gdx.graphics.getHeight();
+
 		batch = new SpriteBatch();
 		img = new Texture("sprite-test.png");
 		sprite = new Sprite(img);
@@ -25,8 +36,13 @@ public class GFS extends ApplicationAdapter {
 		sprite.setRotation(0f);
 		sprite.setScale(10f, 6f);
 
+        glyphLayout = new GlyphLayout();
+
+
         font = new BitmapFont(Gdx.files.internal("fonts/my_font.fnt"));
         myText = "Hola, como estan!";
+        myText_2 = "Texto 2";
+        glyphLayout.setText(font, myText_2);
 	}
 
 	@Override
@@ -34,8 +50,12 @@ public class GFS extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+        // Draw the sprite
 		batch.draw(sprite, sprite.getX(), sprite.getY(), sprite.getWidth() / 2, sprite.getHeight() / 2, sprite.getWidth(), sprite.getHeight(), sprite.getScaleX(), sprite.getScaleY(), sprite.getRotation());
-        font.draw(batch, myText, Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
+        // Draw my text
+        font.draw(batch, myText, xPos / 2, yPos / 2);
+        // Draw my text with the glyphLayout bounds
+        font.draw(batch, glyphLayout, xPos / 2 - glyphLayout.width / 2 , yPos / 2 - glyphLayout.height / 2);
 		batch.end();
 	}
 	
