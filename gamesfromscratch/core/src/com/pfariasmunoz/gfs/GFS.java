0ea7470ea6cd14da1126bcb98ce2964ab5c6f956
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-09 11:57:30 -0300

    Change Texture for Sprite

--- gamesfromscratch/core/src/com/pfariasmunoz/gfs/GFS.java
+++ gamesfromscratch/core/src/com/pfariasmunoz/gfs/GFS.java
@@ -4,16 +4,21 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GFS extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("sprite-test.png");
+		sprite = new Sprite(img);
+		sprite.setPosition(Gdx.graphics.getWidth() / 2 - sprite.getWidth() / 2,
+						   Gdx.graphics.getHeight() / 2 - sprite.getHeight() / 2);
 	}
 
 	@Override
@@ -21,7 +26,7 @@ public class GFS extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, Gdx.graphics.getWidth() / 2 - img.getWidth() / 2, Gdx.graphics.getHeight() / 2 - img.getHeight() / 2);
+		batch.draw(sprite, sprite.getX(), sprite.getY());
 		batch.end();
 	}
 	
