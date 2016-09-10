e6768b0d9efd01039b711960022e6d474e6efcd6
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-10 17:43:44 -0300

    Add camera demo

---
+++ gamesfromscratch_2/html/src/com/pfariasmunoz/camerademo/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.pfariasmunoz.camerademo.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.pfariasmunoz.camerademo.CameraDemo;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new CameraDemo();
+        }
+}
