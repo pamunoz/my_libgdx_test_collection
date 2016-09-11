62a6c6d65f5feac3e4b5356c6d50895ffc66be55
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-10 21:11:17 -0300

    Add camera and center it

--- gamesfromscratch_2/core/src/com/pfariasmunoz/camerademo/CameraDemo.java
+++ gamesfromscratch_2/core/src/com/pfariasmunoz/camerademo/CameraDemo.java
@@ -3,6 +3,7 @@ package com.pfariasmunoz.camerademo;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,18 +11,23 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class CameraDemo extends ApplicationAdapter {
 	SpriteBatch batch;
 	Sprite sprite;
+	OrthographicCamera camera;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		sprite = new Sprite(new Texture(Gdx.files.internal("sample.png")));
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.translate(camera.viewportWidth / 2, camera.viewportHeight / 2);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
 		batch.begin();
+		batch.setProjectionMatrix(camera.combined);
 		sprite.draw(batch);
 		batch.end();
 	}
