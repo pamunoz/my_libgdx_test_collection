2dd909a61f538b74deef07e2768488c19bd678e5
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-15 22:13:27 -0300

    Draw spiral

--- my_tetris/core/src/com/pfariasmunoz/mytetris/MyTetris.java
+++ my_tetris/core/src/com/pfariasmunoz/mytetris/MyTetris.java
@@ -3,31 +3,74 @@ package com.pfariasmunoz.mytetris;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * TODO: Start here
+ *
+ * In this exercise we have a project that draws a number of concentric rectangles as specified in
+ * the COILS constant. The space between the rectangles is given by xStep and yStep.
+ *
+ * The rectangles are drawn using four lines between five points. Your task is to adjust the first
+ * and last point such that each rectangle turns into a coil that meets up with the neighboring
+ * coils inside and outside of it.
+ */
 
 public class MyTetris extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    // How many rectangles/coils to draw
+    private static final int COILS = 20;
+    int screenWidth;
+    int screenHeight;
+    int xStep;
+    int yStep;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    ShapeRenderer shapeRenderer;
+
+    @Override
+    public void create() {
+        shapeRenderer = new ShapeRenderer();
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
+        xStep = screenWidth / 2 / COILS;
+        yStep = screenHeight / 2 / COILS;
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        shapeRenderer.dispose();
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        shapeRenderer.begin(ShapeType.Line);
+
+        for (int i = 1; i < COILS; i++) {
+
+            int xOffset = xStep * i;
+            int yOffset = yStep * i;
+
+            // TODO: Make this coil reach back to the outer coil
+            Vector2 point1 = new Vector2(xOffset - xStep, yOffset);
+            Vector2 point2 = new Vector2(screenWidth - xOffset, yOffset);
+            Vector2 point3 = new Vector2(screenWidth - xOffset, screenHeight - yOffset);
+            Vector2 point4 = new Vector2(xOffset, screenHeight - yOffset);
+
+            // TODO: Make this coil stop before connecting back to itself
+            Vector2 point5 = new Vector2(xOffset, yOffset + yStep);
+
+            shapeRenderer.line(point1, point2);
+            shapeRenderer.line(point2, point3);
+            shapeRenderer.line(point3, point4);
+            shapeRenderer.line(point4, point5);
+        }
+        shapeRenderer.end();
+    }
 }
+
+// TODO: Challenge - Add truncated corners to the spiral
---
+++ my_tetris/core/src/com/pfariasmunoz/mytetris/util/Constants.java
@@ -0,0 +1,10 @@
+package com.pfariasmunoz.mytetris.util;
+
+import com.badlogic.gdx.Gdx;
+
+public class Constants {
+    public static final float SCREEN_WIDTH = Gdx.graphics.getWidth();
+    public static final float SCREEN_HEIGHT = Gdx.graphics.getHeight();
+    public static final float SQUARE_SIDE = SCREEN_WIDTH / 12;
+
+}
