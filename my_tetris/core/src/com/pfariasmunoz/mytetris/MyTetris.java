b59ae299a044591ea89991393000647a6be8c38b
Pablo Munoz <pfariasmunoz@gmail.com>
2016-09-16 21:40:26 -0300

    Add Dragon Curve fractal

--- my_tetris/core/src/com/pfariasmunoz/mytetris/MyTetris.java
+++ my_tetris/core/src/com/pfariasmunoz/mytetris/MyTetris.java
@@ -2,23 +2,31 @@ package com.pfariasmunoz.mytetris;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 
 /*
 TODO: Start here
-The Cantor gasket is a fractal where we start with a white square. We divide that square up into a 3x3 grid of smaller squares, then remove the middle square. Finally, we repeat the process on each of the remaining 8 squares.
+The Dragon Curve is a fractal made by a single line. It is formed of a series of turns, which can be constructed in the following way:
+0: L
+1: L + L + R
+2: LLR + L + LRR
+3: LLRLLRR + L + LLRRLRR
+The nth dragon curve is the n-1th dragon curve plus L, plus the n-1th dragon curve reversed and reflected.
+In this project we have split up the tasks of generating and drawing the dragon curve into separate classes.
  */
 
 public class MyTetris extends ApplicationAdapter {
-    ShapeRenderer shapeRenderer;
-    // TODO: Set a constant for how many recursions to draw. 5 is a good place to start
-    int recursions = 6;
+    private float[] dragonCurve;
+    // Any more than 10 and we'll need to break up the polyline into multiple lines
+    private static final int RECURSIONS = 10;
+
+    private ShapeRenderer shapeRenderer;
 
     @Override
     public void create () {
+        dragonCurve = DragonCurveGenerator.generateDragonCurve(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), RECURSIONS);
         shapeRenderer = new ShapeRenderer();
     }
 
@@ -26,82 +34,8 @@ public class MyTetris extends ApplicationAdapter {
     public void render () {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        // Finds a good place to draw our fractal
-        // Rectangle has members x,y for the lower left corner, and width and height
-        Rectangle bounds = findLargestSquare();
-
-        // TODO: Begin a filled shapeRenderer batch
-        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-
-        // TODO: Draw a white square matching the bounds
-        shapeRenderer.setColor(Color.WHITE);
-        shapeRenderer.rect(bounds.x, bounds.y, bounds.width, bounds.height);
-
-        // TODO: Set the working color to black, and call punchCantorGasket with the bounds
-        shapeRenderer.setColor(Color.BLACK);
-        punchCantorGasket(bounds.x, bounds.y, bounds.width, recursions);
-
-        // TODO: End the batch
+        shapeRenderer.begin(ShapeType.Line);
+        shapeRenderer.polyline(dragonCurve);
         shapeRenderer.end();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        shapeRenderer = new ShapeRenderer();
-    }
-
-
-    private void punchCantorGasket(float x, float y, float size, int recursions){
-
-        // Note that size means the height and width of the square
-        // TODO: Base case, if recursions = 0, return
-        if (recursions == 0) return;
-        float smallSize = size / 3.0f;
-        float newSmallSize = smallSize / 3;
-
-        // TODO: Draw a black square in the middle square
-
-        shapeRenderer.rect(x + smallSize, y + smallSize, smallSize, smallSize);
-
-        // TODO: Call punchCantorGasket on all 8 other squares
-        recursions--;
-
-
-        // bottom line
-        punchCantorGasket(x, y, smallSize, recursions);
-        punchCantorGasket(x + smallSize, y, smallSize, recursions);
-        punchCantorGasket(x + smallSize * 2, y, smallSize, recursions);
-
-        // top line
-        punchCantorGasket(x, y + smallSize * 2, smallSize, recursions);
-        punchCantorGasket(x + smallSize, y + smallSize * 2, smallSize, recursions);
-        punchCantorGasket(x + smallSize * 2, y + smallSize * 2, smallSize, recursions);
-
-        // left and right square
-        punchCantorGasket(x, y + smallSize, smallSize, recursions);
-        punchCantorGasket(x + smallSize * 2, y + smallSize, smallSize, recursions);
-
-
-        //punchCantorGasket(x, y, size, recursions);
-    }
-
-    private Rectangle findLargestSquare(){
-        Rectangle largestSquare = new Rectangle();
-        float screenWidth = Gdx.graphics.getWidth();
-        float screenHeight = Gdx.graphics.getHeight();
-
-        if (screenWidth > screenHeight){
-            largestSquare.x = (screenWidth - screenHeight)/2;
-            largestSquare.y = 0;
-            largestSquare.width = screenHeight;
-            largestSquare.height = screenHeight;
-        } else {
-            largestSquare.x = 0;
-            largestSquare.y = (screenHeight - screenWidth)/2;
-            largestSquare.width = screenWidth;
-            largestSquare.height = screenWidth;
-        }
-        return largestSquare;
     }
 }
