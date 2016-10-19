02bd8c395c63badd1882ad5bfba6e7ef3f4afefc
Pablo Munoz <pfariasmunoz@gmail.com>
2016-10-19 11:21:59 -0300

    Remove unnecesary files

---
+++ udacity/ud405/IciclesGame/core/src/com/udacity/gamedev/icicles/Constants.java
@@ -0,0 +1,21 @@
+package com.udacity.gamedev.icicles;
+
+import com.badlogic.gdx.graphics.Color;
+
+public class Constants {
+
+    // TODO: Add a constant for the world size
+    public static final float WORLD_SIZE = 0f;
+
+    // TODO: Add a constant for the background color of the world
+    public static final Color WORLD_BACKGROUND_COLOR = new Color(0, 0, 0, 1);
+
+    // TODO: Add a constant for the height of the icicle
+    public static final float ICICLE_HEIGHT = 1f;
+
+    // TODO: Add a constant for the width of the icicle
+    public static final float ICICLE_WIDTH = 1f;
+
+    // TODO: Add a constant for the color of the icicles
+    public static final Color ICICLES_COLOR = new Color(1f, 1f, 1f, 1f);
+}
