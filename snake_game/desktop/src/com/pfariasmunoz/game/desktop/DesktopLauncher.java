9bd7a51bae32d2ce2de708b18724c703beddd6df
Pablo Munoz <pfariasmunoz@gmail.com>
2016-10-20 19:19:11 -0300

    Add snake_game project

---
+++ snake_game/desktop/src/com/pfariasmunoz/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.pfariasmunoz.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pfariasmunoz.game.SnakeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SnakeGame(), config);
+	}
+}
